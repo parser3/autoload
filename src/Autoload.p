e212de8e77b5bd86c4541e6ba53fb756697f7921
Leonid Knyazev <leonid@knyazev.me>
2016-11-18 19:28:55 +0300

    Refactoring: - move parser class to Autoload.p file, for direct usage - update path resolver for Posix and Win system (CLI and Apapche2 testing)

---
+++ src/Autoload.p
@@ -0,0 +1,492 @@
+###############################################################################
+# $ID: Autoload.p, Leonid 'n3o' Knyazev $
+###############################################################################
+@CLASS
+Parser/Autoload
+
+
+@OPTIONS
+locals
+
+
+###############################################################################
+# @PUBLIC
+###############################################################################
+@create[params][locals]
+# @{string} [root] Document root.
+$self.root[/]
+
+# @{table} [files] Autoloaded files.
+$self.files[^table::create{hash:path:used}[
+	$.separator[:]
+]]
+
+# @{table} [prefixes] Registred prefixes.
+$self.prefixes[^table::create{name:path}[
+	$.separator[:]
+]]
+
+^self._configure[$params]
+#end @create[]
+
+
+###############################################################################
+@use[class;params][locals]
+$params[^hash::create[$params]]
+
+$class[^class.trim[]]
+
+^try{
+	^self._use[$class;$params]
+}{
+	$exception.handled(true)
+
+	$class[^self._normalizeClass[$class]]
+
+	$path[^self._findPath[$class]]
+	$name[^self._findName[$class]]
+	$type[^self._findType[$class]]
+
+	$path[${path}/${name}]
+
+	$prefix[^self._findPrefix[$path]]
+
+	^if(def $prefix){
+		$path[^path.match[$prefix.name][gi]{}]
+		$path[^path.trim[both;/]]
+	}{
+		$prefix[^self._findPrefix[*]]
+	}
+
+	^prefix.menu{
+		^if(-f "${prefix.path}/${name}.${type}"){
+			^self._use[${prefix.path}/${name}.${type}][$params]
+			^break[]
+		}(-f "${prefix.path}/${path}/${name}.${type}"){
+			^self._use[${prefix.path}/${path}/${name}.${type}][$params]
+			^break[]
+		}($prefix.name ne "*" && -f "${prefix.path}/${prefix.name}/${name}.${type}"){
+			^self._use[${prefix.path}/${prefix.name}/${name}.${type}][$params]
+			^break[]
+		}($prefix.name ne "*" && -f "${prefix.path}/${prefix.name}/${path}/${name}.${type}"){
+			^self._use[${prefix.path}/${prefix.name}/${path}/${name}.${type}][$params]
+			^break[]
+		}
+	}
+}
+#end @use[]
+
+
+###############################################################################
+@autouse[path]
+^try{
+	^self.use[$path]
+}{
+	$exception.handled(true)
+
+	^if($self._autouse is junction){
+		^self._autouse[$path]
+	}
+}
+#end @autouse[]
+
+
+###############################################################################
+@register[prefix;path;force][locals]
+$force(^force.bool(false))
+
+$prefix[^self._normalizePrefix[$prefix]]
+
+^if($force || $prefix eq "*" || !^self.prefixes.locate[name;$prefix]){
+	$path[^self._toPosix[$path]]
+	$path[^self._relative[$self.root;$path]]
+
+	^if(^path.left(1) ne "/"){
+		$path[/$path]
+	}
+
+	^if(!^MAIN:CLASS_PATH.locate[path;$path]){
+		^MAIN:CLASS_PATH.append{$path}
+	}
+
+	^if($prefix eq ""){
+		$prefix[*]
+	}
+
+	^self.prefixes.append[
+		$.name[$prefix]
+		$.path[$path]
+	]
+}
+#end @register[]
+
+
+###############################################################################
+@include[path;hash][locals]
+^if(!def $hash){
+	$hash[^math:md5[$path]]
+}
+
+^if(!^self.files.locate[hash;$hash]){
+	$used[false]
+
+	$path[^self._toPosix[$path]]
+	$path[^self._relative[$self.root;$path]]
+
+	^if(^path.left(1) ne "/"){
+		$path[/$path]
+	}
+
+	^if(-f "${path}"){
+		^self._use[$path]
+		$used[true]
+	}
+
+	^self.files.append[
+		$.hash[$hash]
+		$.path[$path]
+		$.used[$used]
+	]
+}
+#end @include[]
+
+
+
+###############################################################################
+# @PRIVATE
+###############################################################################
+@_configure[params][locals]
+$params[^hash::create[$params]]
+
+# find Document root.
+$self.root[^self._findRoot[$params.root]]
+
+# find $MAIN:CLASS_PATH
+^if(!def $MAIN:CLASS_PATH){
+	$MAIN:CLASS_PATH[^table::create{path}]
+}($MAIN:CLASS_PATH is string){
+	$MAIN:CLASS_PATH[^table::create{path^#0A${MAIN:CLASS_PATH}}]
+}
+
+# extend @MIAN:use[]
+$self._use[$MAIN:use]
+
+^process[$MAIN:CLASS]{@use[path^;params][locals]
+	^^MAIN:AUTOLOAD.use[^$path^;^$params]
+}
+
+# extend @MIAN:autouse[]
+$self._autouse[$MAIN:autouse]
+
+^process[$MAIN:CLASS]{@autouse[path][locals]
+	^^MAIN:AUTOLOAD.autouse[^$path^]
+}
+
+# process autoload files
+^if(def $params.files && $params.files is table){
+	^params.files.menu{
+		^self.include[$params.files.path;$params.files.hash]
+	}
+}
+
+# process namespaces
+^if(def $params.prefixes && $params.prefixes is table){
+	^params.prefixes.menu{
+		^self.register[$params.prefixes.name;$params.prefixes.path]
+	}
+}
+#end @_configure[]
+
+
+###############################################################################
+@_findRoot[root][locals]
+$result[$root]
+
+^if(def $env:PWD){
+	$result[$env:PWD]
+}(def $env:DOCUMENT_ROOT_VIRTUAL){
+	$result[$env:DOCUMENT_ROOT_VIRTUAL]
+}(def $env:DOCUMENT_ROOT){
+	$result[$env:DOCUMENT_ROOT]
+}
+
+$result[^self._toPosix[$result]]
+
+^if(^result.left(1) ne "/"){
+	$result[/$result]
+}
+#end @_findRoot[]
+
+
+###############################################################################
+@_findPath[class][locals]
+$result[^file:dirname[$class]]
+#end @_findPath[]
+
+
+###############################################################################
+@_findName[class][locals]
+$result[^file:justname[$class]]
+#end @_findName[]
+
+
+###############################################################################
+@_findType[class][locals]
+$result[^file:justext[$class]]
+
+^if(!def $result){
+	$result[p]
+}
+#end @_findType[]
+
+
+###############################################################################
+@_findPrefix[path][locals]
+$path[^path.trim[both;\/.]]
+
+^if(^self.prefixes.locate[name;$path]){
+	$prefix[^self.prefixes.select($self.prefixes.name eq $path)]
+}{
+	$_parts[^path.split[/;r]]
+
+	^_parts.menu{
+		$_piece[${_piece}/${_parts.piece}]
+		$_prefix[^path.match[$_piece][gi]{}]
+
+		^if(^self.prefixes.locate[name;$_prefix]){
+			$prefix[^self.prefixes.select($self.prefixes.name eq $_prefix)]
+			^break[]
+		}
+	}
+}
+
+$result[$prefix]
+#end @_findPrefix[]
+
+
+###############################################################################
+@_toPosix[path][locals]
+$result[$path]
+
+^if(def $result){
+	$result[^result.trim[]]
+
+	^if(^result.length[] > 2 && ^result.mid(1;1) eq ":" && (^result.mid(2;1) eq "/" || ^result.mid(2;1) eq "\")){
+		$result[^result.split[:;rh]]
+		$result[$result.0]
+	}
+}
+#end @_toPosix[]
+
+
+###############################################################################
+@_normalizePrefix[prefix][locals]
+$result[^prefix.replace[_;/]]
+$result[^result.replace[\;/]]
+$result[^result.trim[both;\/.]]
+#end @_normalizePrefix[]
+
+
+###############################################################################
+@_normalizeClass[class][locals]
+$result[$class]
+
+^if(def $result){
+	$result[^result.replace[_;/]]
+	$result[^result.trim[both;\/]]
+}
+#end @_normalizeClass[]
+
+
+###############################################################################
+@_relative[from;to][locals]
+$result[]
+
+$to[^self._resolve[$from;$to]]
+
+^if($from ne $to){
+	^for[fromStart](1;^from.length[]){
+		^if(^from.mid($fromStart;1) ne "/"){
+			^break[]
+		}
+	}
+	$fromEnd(^from.length[])
+	$fromLen($fromEnd - $fromStart)
+
+	^for[toStart](1;^to.length[]){
+		^if(^to.mid($toStart;1) ne "/"){
+			^break[]
+		}
+	}
+	$toEnd(^to.length[])
+	$toLen($toEnd - $toStart)
+
+	$length(^if($fromLen < $toLen){$fromLen}{$toLen})
+	$lastCommonSep(-1)
+
+	^for[i](0;$length){
+		^if($i == $length){
+			^if($toLen > $length){
+				^if(^to.mid(($toStart + $i);1) eq "/"){
+					$result[^to.mid(($toStart + $i + 1);^to.length[])]
+				}($i == 0){
+					$result[^to.mid(($toStart + $i);^to.length[])]
+				}
+			}($fromLen > $length){
+				^if(^from.mid(($fromStart + $i);1) eq "/"){
+					$lastCommonSep($i)
+				}($i == 0){
+					$lastCommonSep(0)
+				}
+			}
+
+			^break[]
+		}
+
+		$fromCode[^from.mid(($fromStart + $i);1)]
+		$toCode[^to.mid(($toStart + $i);1)]
+
+		^if($fromCode ne $toCode){
+			^break[]
+		}($fromCode eq "/"){
+			$lastCommonSep($i)
+		}
+	}
+
+	^if(!def $result){
+		$return[]
+		$index($fromStart + $lastCommonSep + 1)
+
+		^while($index <= $fromEnd){
+			^if($index == $fromEnd || ^from.mid($index;1) eq "/"){
+				^if(^return.length[] == 0){
+					$return[..]
+				}{
+					$return[${return}/..]
+				}
+			}
+
+			^index.inc[]
+		}
+
+
+		^if(^return.length[] > 0){
+			$result[${return}^to.mid(($toStart + $lastCommonSep);^to.length[])]
+		}{
+			^toStart.inc($lastCommonSep)
+
+			^if(^to.mid($toStart;1) eq "/"){
+				^toStart.inc[]
+			}
+
+			$result[^to.mid($toStart;^to.length[])]
+		}
+	}
+}
+#end @_relative[]
+
+
+###############################################################################
+@_resolve[*paths]
+$result[]
+
+$paths[^hash::create[$paths]]
+$count(^paths.count[] - 1)
+
+$isAbsolute(false)
+
+^if($count >= 0){
+	^while($count >= 0){
+		$path[^paths.at($count)[value]]
+		$path[^path.trim[]]
+
+		^if(^path.length[] == 0){
+			^count.dec[]
+			^continue[]
+		}
+
+		$result[${path}^if(def $result){/${result}}]
+
+		$isAbsolute(^self._isAbsolute[$path])
+
+		^if($isAbsolute){
+			^break[]
+		}{
+			^count.dec[]
+		}
+	}
+
+	$result[^self._normalize[$result;!$isAbsolute]]
+
+	^if($isAbsolute){
+		^if(^result.length[] > 0){
+			$result[/${result}]
+		}{
+			$result[/]
+		}
+	}(^result.length[] > 0){
+		$result[${self.root}/${result}]
+	}{
+		$result[$self.root]
+	}
+}{
+	$result[$self.root]
+}
+#end @_resolve[]
+
+
+###############################################################################
+@_normalize[path;isAbsolute]
+$result[]
+
+$isAbsolute(^isAbsolute.bool(^self._isAbsolute[$path]))
+
+$path[^path.trim[both;/]]
+
+$parts[^path.split[/;l]]
+$parts[^parts.select(def ^parts.piece.trim[] && ^parts.piece.trim[] ne ".")]
+
+$paths[^table::create{path}]
+
+^parts.menu{
+	$part[^parts.piece.trim[]]
+
+	^if($part eq '..'){
+		^if($isAbsolute){
+			^if(^paths.count[]){
+				^paths.delete[]
+			}
+		}{
+			^if(^paths.count[]){
+				^if($paths.path eq '..'){
+					^paths.append{$part}
+				}{
+					^paths.delete[]
+				}
+			}{
+				^paths.append{$part}
+			}
+		}
+	}{
+		^paths.append{$part}
+	}
+
+	^paths.offset[set]($paths - 1)
+}
+
+$result[^paths.menu{$paths.path}[/]]
+#end @_normalize[]
+
+
+###############################################################################
+@_isAbsolute[path][locals]
+$result(false)
+
+^if(def $path){
+	^if(^path.left(1) eq "/" || ^path.left(1) eq "\"){
+		$result(true)
+	}(^path.length[] > 2 && ^path.mid(1;1) eq ":" && (^path.mid(2;1) eq "/" || ^path.mid(2;1) eq "\")){
+		$result(true)
+	}
+}
+#end @_isAbsolute[]
--- src/Plugin.php
+++ src/Plugin.php
@@ -42,6 +42,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     protected $filesystem;
 
 
+
     /**
      * @param Composer $composer
      * @param IOInterface $io
@@ -96,16 +97,16 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         // Project root directory.
         $rootDir = $this->filesystem->normalizePath(realpath(realpath(getcwd())));
 
-        // Process {"autoload":{"files":[]}}
+        // Get autoload files
         $files = $this->getAutoloadFiles($vendorDir);
 
-        // Process {"autoload":{"psr-0":[]}} and {"autoload":{"psr-4":[]}}
-        $namespaces = $this->getAutoloadNamespaces($vendorDir);
+        // Get autoload prefixes
+        $prefixes = $this->getAutoloadPrefixes($vendorDir);
 
         // Get parser/autoload version
         $version = $this->getAutoloadVersion($vendorDir);
 
-        return $this->generateParserClass($files, $namespaces, $version, $rootDir);
+        return $this->generateParserClass($files, $prefixes, $version, $rootDir);
     }
 
 
@@ -115,19 +116,21 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      */
     private function getAutoloadFiles($vendorDir)
     {
-        $result = "";
-
         if (file_exists($vendorDir . '/composer/autoload_files.php')) {
             $files = require $vendorDir . '/composer/autoload_files.php';
 
             if (!empty($files)) {
+                $result = "\n";
+
                 foreach ($files as $hash => $path) {
-                    $result .= $this->parsePath($path) . "\n";
+                    $result .= $hash ."|". $this->parsePath($path) . "\n";
                 }
+
+                return $result;
             }
         }
 
-        return $result;
+        return "";
     }
 
 
@@ -135,31 +138,33 @@ class Plugin implements PluginInterface, EventSubscriberInterface
      * @param $vendorDir
      * @return string
      */
-    private function getAutoloadNamespaces($vendorDir)
+    private function getAutoloadPrefixes($vendorDir)
     {
-        $result = "";
-
-        $namespaces = array();
+        $prefixes = array();
 
         if (file_exists($vendorDir . '/composer/autoload_namespaces.php')) {
-            $namespaces = array_merge($namespaces, require $vendorDir . '/composer/autoload_namespaces.php');
+            $prefixes = array_merge($prefixes, require $vendorDir . '/composer/autoload_namespaces.php');
         }
 
         if (file_exists($vendorDir . '/composer/autoload_psr4.php')) {
-            $namespaces = array_merge($namespaces, require $vendorDir . '/composer/autoload_psr4.php');
+            $prefixes = array_merge($prefixes, require $vendorDir . '/composer/autoload_psr4.php');
         }
 
-        if (!empty($namespaces)) {
-            foreach ($namespaces as $namespace => $paths) {
+        if (!empty($prefixes)) {
+            $result = "\n";
+
+            foreach ($prefixes as $prefix => $paths) {
                 foreach ($paths as $path) {
                     if (!$this->isDumper($path)) {
-                        $result .= $this->parseNamespace($namespace) . "=" . $this->parsePath($path) . "\n";
+                        $result .= $this->parsePrefix($prefix) ."|". $this->parsePath($path) . "\n";
                     }
                 }
             }
+
+            return $result;
         }
 
-        return $result;
+        return "";
     }
 
 
@@ -190,28 +195,28 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
     /**
      * @param $path
-     * @param $rootDir
      * @return mixed
      */
     private function parsePath($path)
     {
-        return str_replace("\\", "/", $path);;
+        return str_replace("\\", "/", $path);
     }
 
 
     /**
-     * @param $namespace
+     * @param $prefix
      * @return mixed
      */
-    private function parseNamespace($namespace)
+    private function parsePrefix($prefix)
     {
-        $namespace = trim($namespace, "\\");
+        $prefix = trim($prefix, "\\");
+        $prefix = trim($prefix, "_");
 
-        if (empty($namespace)) {
-            $namespace = "*";
+        if (empty($prefix)) {
+            $prefix = "*";
         }
 
-        return str_replace("\\", "/", $namespace);
+        return str_replace(array("\\", "_"), "/", $prefix);
     }
 
 
@@ -225,7 +230,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     }
 
 
-    private function generateParserClass($files, $namespaces, $version, $rootDir)
+
+    private function generateParserClass($files, $prefixes, $version, $rootDir)
     {
         return <<<AUTOLOAD
 ###############################################################################
@@ -233,326 +239,26 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 ###############################################################################
 # \$ID: autoload.p, v$version, Leonid 'n3o' Knyazev $
 ###############################################################################
-@auto[base][locals]
-\$MAIN:COMPOSER[^COMPOSER::create[\$base]]
+@USE
+parser/autoload/src/Autoload.p
+
+
+###############################################################################
+@auto[][locals]
+\$files[^table::create{hash|path$files}[
+	$.separator[|]
+]]
+
+\$prefixes[^table::create{name|path$prefixes}[
+	$.separator[|]
+]]
+
+\$MAIN:AUTOLOAD[^Parser/Autoload::create[
+	\$.root[$rootDir]
+	\$.files[\$files]
+	\$.prefixes[\$prefixes]
+]]
 #end @auto[]
-
-
-###############################################################################
-# Parser3 Class Loader
-###############################################################################
-@CLASS
-COMPOSER
-
-
-@OPTIONS
-locals
-
-
-###############################################################################
-# @PUBLIC
-###############################################################################
-@create[base][locals]
-# @{string} [_root] Document root.
-\$self.root[^self._getRoot[]]
-
-# find \$MAIN:CLASS_PATH
-^if(!def \$MAIN:CLASS_PATH){
-	\$MAIN:CLASS_PATH[^table::create{path}]
-}(\$MAIN:CLASS_PATH is string){
-	\$MAIN:CLASS_PATH[^table::create{path^#0A\${MAIN:CLASS_PATH}}]
-}
-
-# @{table} [_namespaces] Registred namespaces.
-\$self.namespaces[^table::create{name:path}[ $.separator[:] ]]
-
-# extend @MIAN:use[]
-\$self._use[\$MAIN:use]
-
-^process[\$MAIN:CLASS]{@use[path^;params][locals]
-	^^MAIN:COMPOSER.use[^\$path^;^\$params]
-}
-
-# extend @MIAN:autouse[]
-\$self._autouse[\$MAIN:autouse]
-
-^process[\$MAIN:CLASS]{@autouse[path][locals]
-	^^MAIN:COMPOSER.autouse[^\$path^]
-}
-
-^self._update[]
-#end @create[]
-
-
-###############################################################################
-@use[class;params][locals]
-\$params[^hash::create[\$params]]
-
-^try{
-	^self._use[\$class;\$params]
-}{
-	\$exception.handled(true)
-
-	^rem{*** find file type ***}
-	\$type[^file:justext[\$class]]
-
-	^rem{** remove file type ***}
-	^if(def \$type){
-		\$class[^class.match[\.\$type][gi]{}]
-	}{
-		\$type[p]
-	}
-
-	^rem{*** find namespace ***}
-	\$found(false)
-	
-	\$namespace[\$class]
-
-	^if(^self.namespaces.locate[name;\$namespace]){
-		\$found(true)
-	}{
-		\$_parts[^class.split[/;r]]
-
-		^_parts.menu{
-			\$piece[\${piece}/\${_parts.piece}]
-			\$namespace[^class.match[\$piece][gi]{}]
-
-			^if(^self.namespaces.locate[name;\$namespace]){
-				\$found(true)
-				^break[]
-			}
-		}
-	}
-
-	^rem{*** find file name ***}
-	\$name[^class.match[\$namespace][gi]{}]
-	\$name[^name.trim[both;/]]
-
-	^if(!def \$name){
-		\$name[^file:basename[\$class]]
-	}
-
-	^if(\$found){
-		\$paths[^self.namespaces.select(\$self.namespaces.name eq \$namespace)]
-	}{
-		\$paths[^self.namespaces.select(\$self.namespaces.name eq "*")]
-	}
-	
-	^if(\$paths){
-		^paths.menu{
-			\$path[\$paths.path]
-
-			^rem{*** find class file ***}
-			^if(-f "\${path}/\${name}.\${type}"){
-				^self._use[\${path}/\${name}.\${type}]
-				^break[]
-			}(-f "\${path}/\${namespace}.\${type}"){
-				^self._use[\${path}/\${namespace}.\${type}]
-				^break[]
-			}(-f "\${path}/\${namespace}/\${name}.\${type}"){
-				^self._use[\${path}/\${namespace}/\${name}.\${type}]
-				^break[]
-			}
-		}
-	}
-}
-#end @use[]
-
-
-###############################################################################
-@autouse[path]
-^try{
-	^self.use[\$path]
-}{
-	\$exception.handled(true)
-
-	^if(\$self._autouse is junction){
-		^self._autouse[\$path]
-	}
-}
-#end @autouse[]
-
-
-
-###############################################################################
-# @PRIVATE
-###############################################################################
-@_update[][locals]
-# process auto include files
-\$_includes[^self._getIncludes[]]
-
-^if(\$_includes){
-	^_includes.menu{
-		\$path[^self._normalizePath[\$_includes.path]]
-		\$path[^self._relative[\$self.root;\$path]]
-		
-		^if(^path.left(1) ne "/"){
-			\$path[/\$path]
-		}
-		
-		^if(-f "\${path}"){
-			^self._use[\$path]
-		}
-	}
-}
-
-# process namespaces
-\$_namespaces[^self._getNamespaces[]]
-
-^if(\$_namespaces){
-	^_namespaces.menu{
-		\$name[^_namespaces.name.trim[]]
-		
-		\$path[^self._normalizePath[\$_namespaces.path]]
-		\$path[^self._relative[\$self.root;\$path]]
-		
-		^if(^path.left(1) ne "/"){
-			\$path[/\$path]
-		}
-
-		^if(!^MAIN:CLASS_PATH.locate[path;\$path]){
-			^MAIN:CLASS_PATH.append{\$path}
-		}
-		
-		^if(\$name eq ""){
-			\$name[*]
-		}
-
-		^self.namespaces.append{\$name	\$path}
-	}
-}
-#end @_update[]
-
-
-###############################################################################
-@_getRoot[][locals]
-\$result[$rootDir]
-
-# find \$_root
-^if(def \$env:PWD){
-	\$result[\$env:PWD]
-}(def \$env:DOCUMENT_ROOT_VIRTUAL){
-	\$result[\$env:DOCUMENT_ROOT_VIRTUAL]
-}(def \$env:DOCUMENT_ROOT){
-	\$result[\$env:DOCUMENT_ROOT]
-}
-
-\$result[^self._normalizePath[\$result]]
-#end @_getRoot[]
-
-
-###############################################################################
-@_normalizePath[path][locals]
-\$result[\$path]
-
-^if(def \$result){
-	\$result[^result.trim[]]
-	\$result[^result.split[:;rh]]
-	\$result[/^result.0.trim[both;\/]]
-}
-#end @_normalizePath[]
-
-
-###############################################################################
-@_relative[from;to][locals]
-\$result[]
-
-^if(\$from ne \$to){
-	^for[fromStart](1;^from.length[]){
-		^if(^from.mid(\$fromStart;1) ne "/"){
-			^break[]
-		}
-	}
-	\$fromEnd(^from.length[])
-	\$fromLen(\$fromEnd - \$fromStart)
-
-
-	^for[toStart](1;^to.length[]){
-		^if(^to.mid(\$toStart;1) ne "/"){
-			^break[]
-		}
-	}
-	\$toEnd(^to.length[])
-	\$toLen(\$toEnd - \$toStart)
-
-
-	\$length(^if(\$fromLen < \$toLen){\$fromLen}{\$toLen})
-	\$lastCommonSep(-1)
-
-	^for[i](0;\$length){
-		^if(\$i == \$length){
-			^if(\$toLen > \$length){
-				^if(^to.mid((\$toStart + \$i);1) eq "/"){
-					\$result[^to.mid((\$toStart + \$i + 1);^to.length[])]
-				}(\$i == 0){
-					\$result[^to.mid((\$toStart + \$i);^to.length[])]
-				}
-			}(\$fromLen > \$length){
-				^if(^from.mid((\$fromStart + \$i);1) eq "/"){
-					\$lastCommonSep(\$i)
-				}(\$i == 0){
-					\$lastCommonSep(0)
-				}
-			}
-
-			^break[]
-		}
-
-		\$fromCode[^from.mid((\$fromStart + \$i);1)]
-		\$toCode[^to.mid((\$toStart + \$i);1)]
-
-		^if(\$fromCode ne \$toCode){
-			^break[]
-		}(\$fromCode eq "/"){
-			\$lastCommonSep(\$i)
-		}
-	}
-
-	^if(!def \$result){
-		\$return[]
-		\$index(\$fromStart + \$lastCommonSep + 1)
-
-		^while(\$index <= \$fromEnd){
-			^if(\$index == \$fromEnd || ^from.mid(\$index;1) eq "/"){
-				^if(^return.length[] == 0){
-					\$return[..]
-				}{
-					\$return[\${return}/..]
-				}
-			}
-
-			^index.inc[]
-		}
-
-
-		^if(^return.length[] > 0){
-			\$result[\${return}^to.mid((\$toStart + \$lastCommonSep);^to.length[])]
-		}{
-			^toStart.inc(\$lastCommonSep)
-
-			^if(^to.mid(\$toStart;1) eq "/"){
-				^toStart.inc[]
-			}
-
-			\$result[^to.mid(\$toStart;^to.length[])]
-		}
-	}
-}
-#end @_relative[]
-
-
-###############################################################################
-@_getIncludes[][locals]
-\$result[^table::create{path
-$files}]
-#end @_getIncludes[]
-
-
-###############################################################################
-@_getNamespaces[][locals]
-\$result[^table::create{name=path
-$namespaces}[ $.separator[=] ]]
-#end @_getNamespaces[]
 
 AUTOLOAD;
     }
