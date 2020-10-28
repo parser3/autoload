659f2db6820172a697bb541722fd43f7d2d8bd58
Leonid Knyazev <n3o@design.ru>
2020-10-28 13:59:09 +0300

    add deactivate and uninstall methods

--- src/Plugin.php
+++ src/Plugin.php
@@ -54,7 +54,9 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
 
     /**
-     * @param Composer $composer
+     * Apply plugin modifications to Composer
+     *
+     * @param Composer    $composer
      * @param IOInterface $io
      */
     public function activate(Composer $composer, IOInterface $io)
@@ -68,6 +70,36 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
         // Project vendor directory.
         $this->vendorDir = $this->filesystem->normalizePath(realpath(realpath($this->composer->getConfig()->get('vendor-dir'))));
+    }
+
+
+    /**
+     * Remove any hooks from Composer
+     *
+     * This will be called when a plugin is deactivated before being
+     * uninstalled, but also before it gets upgraded to a new version
+     * so the old one can be deactivated and the new one activated.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+
+    }
+
+
+    /**
+     * Prepare the plugin to be uninstalled
+     *
+     * This will be called after deactivate.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+
     }
 
 
